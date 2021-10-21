ab611c1b097ac406605463a50b894a79817de664
azalialoe <azalia.loeciano@gmail.com>
2021-10-21 02:26:22 -0400

    finished all basic styling

--- script.js
+++ script.js
@@ -1,8 +1,28 @@
-const imgContainer = document.querySelectorAll('.imgContainer img');
+const projectOne = document.querySelector('.projectOne');
+const projectTwo = document.querySelector('.projectTwo');
+const projectThree = document.querySelector('.projectThree');
+const projectFour = document.querySelector('.projectFour');
 
-imgContainer.forEach((img) => {
-    img.addEventListener('click', () => {
-        imgContainer.classList.add('active');
-    })
+const overlayOne = document.querySelector('.overlayOne')
+const overlayTwo = document.querySelector('.overlayTwo')
+const overlayThree = document.querySelector('.overlayThree')
+const overlayFour = document.querySelector('.overlayFour')
+
+projectOne.addEventListener('click', () => {
+    overlayOne.classList.toggle('active')
 })
+
+projectTwo.addEventListener('click', () => {
+    overlayTwo.classList.toggle('active')
+})
+
+projectThree.addEventListener('click', () => {
+    overlayThree.classList.toggle('active')
+})
+
+projectFour.addEventListener('click', () => {
+    overlayFour.classList.toggle('active')
+})
+
+
 
